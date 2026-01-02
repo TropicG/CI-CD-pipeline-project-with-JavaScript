32926c424f11a1584c4b0081375cec8a555a7c16
Konstantin Kondov <91820105+TropicG@users.noreply.github.com>
2026-01-02 23:23:57 +0200

    Fix string quotes in server.js

--- src/server.js
+++ src/server.js
@@ -1,23 +1,22 @@
-// src/server.js
-const express = require('express');
-const { add, subtract } = require('./math');
+const express = require("express");
+const { add, subtract } = require("./math");
 
 const app = express();
 const PORT = 3000;
 
-const HOST = '0.0.0.0'; 
+const HOST = "0.0.0.0";
 
-app.get('/', (req, res) => {
-  res.send('Hello from Kubernetes! The app is running.');
+app.get("/", (req, res) => {
+  res.send("Hello from Kubernetes! The app is running.");
 });
 
-app.get('/add', (req, res) => {
+app.get("/add", (req, res) => {
   const a = parseInt(req.query.a) || 0;
   const b = parseInt(req.query.b) || 0;
   res.send(`Result: ${add(a, b)}`);
 });
 
-app.get('/subtract', (req, res) => {
+app.get("/subtract", (req, res) => {
   const a = parseInt(req.query.a) || 0;
   const b = parseInt(req.query.b) || 0;
   res.send(`Result: ${subtract(a, b)}`);
