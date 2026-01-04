9b75da989312a1055500b2e232e426f8fc338384
Konstantin Kondov <91820105+TropicG@users.noreply.github.com>
2026-01-04 09:54:40 +0200

    Update math.test.js

--- tests/math.test.js
+++ tests/math.test.js
@@ -7,3 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('subtracts 5 - 2 to equal 3', () => {
   expect(subtract(5, 2)).toBe(3);
 });
+
+test('multiply 5 * 2 to equal 10', () => {
+  expect(subtract(5, 2)).toBe(10);
+});
